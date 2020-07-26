964672f9b15e0e2e41c30a41c7416a8e1dafda5f
Pritam Basu <34293684+pritambasu@users.noreply.github.com>
2020-07-26 11:17:49 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -191,17 +191,9 @@ $('document').ready(function(){
 		}
 		
 		msgLoop(0);
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-		$('#some_test').fadeIn('slow');
 		
 	});
-	$('#some_test').click(function() {		
-  	window.location='http://www.google.com';
-		
-});
 });
 
 
 
-
-//alert('hello');
